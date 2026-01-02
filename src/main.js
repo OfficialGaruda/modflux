e221b71402844b7357a70cb46c777ce03feb68b4
Shubham Gupta <73527493+OfficialGaruda@users.noreply.github.com>
2026-01-03 05:14:47 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.js'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+
+root.render(
   React.createElement(
     React.StrictMode,
     null,
-    React.createElement(App)
+    React.createElement(App, null)
   )
 )
