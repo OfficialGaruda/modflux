95e09d4fadccab5082d1b6a647111c6f25805029
Shubham Gupta <73527493+OfficialGaruda@users.noreply.github.com>
2026-01-03 05:04:50 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.js'
+import React, { StrictMode } from "https://esm.sh/react@18";
+import ReactDOM from "https://esm.sh/react-dom@18/client";
+import htm from "https://esm.sh/htm";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./App.js";
+
+const html = htm.bind(React.createElement);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  html`
+    <${StrictMode}>
+      <${App} />
+    </${StrictMode}>
+  `
+);
