1cb77f73adb5d61b4cf69ac7bcbc0990d080c27b
Shubham Gupta <73527493+OfficialGaruda@users.noreply.github.com>
2026-01-03 05:05:52 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import React, { StrictMode } from "https://esm.sh/react@18";
 import ReactDOM from "https://esm.sh/react-dom@18/client";
 import htm from "https://esm.sh/htm";
 
-import "./index.css";
 import App from "./App.js";
 
 const html = htm.bind(React.createElement);
