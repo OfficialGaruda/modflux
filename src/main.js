632929c491f1374c5fb524a034b654076026617f
Shubham Gupta <73527493+OfficialGaruda@users.noreply.github.com>
2026-01-03 05:14:18 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
-import React, { StrictMode } from "https://esm.sh/react@18";
-import ReactDOM from "https://esm.sh/react-dom@18/client";
-import htm from "https://esm.sh/htm";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.js'
 
-import App from "./App.js"; // ← this should be line 5 or 6
-
-const html = htm.bind(React.createElement);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  html`
-    <${StrictMode}>
-      <${App} />
-    </${StrictMode}>
-  `
-);
+createRoot(document.getElementById('root')).render(
+  React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(App)
+  )
+)
